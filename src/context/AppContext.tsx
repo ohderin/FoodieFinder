d94ebfcb8d45281d35cc3ea2fb053f562e4e291a
ash <awil678@lsu.edu>
2026-04-27 04:11:46 -0500

    favorites now show
    I needed to edit index, saved, and appcontext in order to get this runnin. When you tap the heart button on the main screen for a restaurant, the item is placed in the saved screen. From there, its short description, open status, and image are displayed alongside the name.

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { type Restaurant } from "../../src/data/sampleRestaurant";
 
 const KEY = "foodie.hasCompletedOnboarding";
 
@@ -8,6 +9,9 @@ type AppContextValue = {
   hasCompletedOnboarding: boolean;
   completeOnboarding: () => Promise<void>;
   resetOnboarding: () => Promise<void>;
+  hearted: Restaurant[];
+  addHeart: (res: Restaurant) => void;
+  removeHeart: (id: string) => void;
 };
 
 const AppContext = createContext<AppContextValue | null>(null);
@@ -15,6 +19,8 @@ const AppContext = createContext<AppContextValue | null>(null);
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [ready, setReady] = useState(false);
   const [hasCompletedOnboarding, setHasCompletedOnboarding] = useState(false);
+  const [hearted, setHearted] = useState<Restaurant[]>([]);
+  
 
   useEffect(() => {
     (async () => {
@@ -37,14 +43,29 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
     setHasCompletedOnboarding(false);
   }, []);
 
+  const addHeart = useCallback((res: Restaurant) => {
+    setHearted((prev) => {
+      // Check for duplicates based on the Restaurant ID
+      if (prev.find((item) => item.id === res.id)) return prev;
+      return [...prev, res];
+    });
+  }, []);
+
+  const removeHeart = useCallback((id: string) => {
+  setHearted((prev) => prev.filter((item) => item.id !== id));
+}, []);
+
   const value = useMemo(
     () => ({
       ready,
       hasCompletedOnboarding,
       completeOnboarding,
       resetOnboarding,
+      hearted,
+      addHeart,
+      removeHeart,
     }),
-    [ready, hasCompletedOnboarding, completeOnboarding, resetOnboarding]
+    [ready, hasCompletedOnboarding, completeOnboarding, resetOnboarding, hearted, addHeart, removeHeart]
   );
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
